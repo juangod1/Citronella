a34678f2a638a2d0f71b1d240655a96881166e94
juangod1 <juan.godfrid@gmail.com>
2018-11-26 14:39:29 -0300

    compilation fix

--- Citronella_compile.sh
+++ Citronella_compile.sh
@@ -2,5 +2,10 @@ if [ -f "$2.c" ]; then
     rm "$2.c";
 fi
 touch "$2.c";
-cat $1 | ./Citronella >> "$2.c";
-gcc "$2.c" -o $2
+
+cat $1 | if ./Citronella >> "$2.c"; then
+  gcc "$2.c" -o $2
+  rm "$2.c"
+else
+    echo "Citronella compilation failed"
+fi
